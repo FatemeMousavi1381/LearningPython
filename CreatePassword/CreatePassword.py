6fb02b6abcca2976d90db61f4d932a994dbf5f24
Fateme <fateme.mousavi1381@gmail.com>
2025-08-10 22:20:43 +0330

    Update CreatePassword.py
    Added a button to copy the generated password to the clipboard.

--- CreatePassword/CreatePassword.py
+++ CreatePassword/CreatePassword.py
@@ -32,10 +32,17 @@ def generate():
         if pwd : 
             result_var.set(pwd)
         
+def copy_password():
+    password = result_var.get()
+    if password:
+        root.clipboard_clear()
+        root.clipboard_append(password)
+        messagebox.showinfo("Copied!", "Password copied to clipboard.")
+
 
 root = tk.Tk()
 root.title('Password Generator')
-root.geometry('400x150')
+root.geometry('400x170')
 
 
 tk.Label(root, text="Enter password length:" , font="bold" , fg="Green" ).pack(pady=5)
@@ -49,6 +56,9 @@ btn.pack(pady=5)
 result_var = tk.StringVar()
 result_label = tk.Label(root, textvariable=result_var, font=("Aria", 12 , "bold"), fg="crimson")
 result_label.pack(pady=5)
+
+btn_copy = tk.Button(root, text="Copy Password", fg="Green", command=copy_password)
+btn_copy.pack()
 
 root.mainloop()
             
